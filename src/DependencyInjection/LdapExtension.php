8b2ee45115609283d9298bc53108d79f7dfb5700
Andrii Yakovlev <yawa20@gmail.com>
2019-01-15 13:08:53 +0000

    Fix configuration & rearrange code

--- src/DependencyInjection/LdapExtension.php
+++ src/DependencyInjection/LdapExtension.php
@@ -6,9 +6,12 @@ use GepurIt\LdapBundle\Ldap\LdapConnection;
 use GepurIt\LdapBundle\Security\LdapUserProvider;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Ldap\Adapter\ExtLdap\Adapter;
 use Symfony\Component\Ldap\Ldap;
+use Symfony\Component\Ldap\LdapInterface;
 
 /**
  * Class LdapExtension
@@ -33,8 +36,32 @@ class LdapExtension extends Extension
         $configuration = $this->getConfiguration($configs, $container);
         $config        = $this->processConfiguration($configuration, $configs);
 
+        $this->initComponent($container, $config);
         $this->initLdapConnection($container, $config);
         $this->initLdapUserProvider($container, $config);
+    }
+
+    private function initComponent(ContainerBuilder $container, array $config)
+    {
+        $adapter = new Definition();
+        $adapter->setClass(Adapter::class);
+        $adapterConfig = [
+            'host' => $config['ldap_host'],
+            'port' => $config['ldap_port'],
+            'encryption' => $config['ldap_encryption'],
+            'options' => [
+                'protocol_version' => 3,
+                'referrals' => false
+            ]
+
+        ];
+        $adapter->setArgument('$config', $adapterConfig);
+        $container->setDefinition(Adapter::class, $adapter);
+
+        $ldapDefinition = new Definition();
+        $ldapDefinition->setClass(Ldap::class);
+        $container->setDefinition(Ldap::class, $ldapDefinition);
+        $container->setAlias(LdapInterface::class, Ldap::class);
     }
 
     private function initLdapConnection(ContainerBuilder $container, array $config)
