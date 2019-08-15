6a7bb1f09b1a95b840e2a1102ef749aa9ecbd4c2
Andrii Yakovlev <yawa20@gmail.com>
2019-08-15 13:01:06 +0000

    remove deprecation

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -24,8 +24,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('ldap');
+        $treeBuilder = new TreeBuilder('ldap');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
